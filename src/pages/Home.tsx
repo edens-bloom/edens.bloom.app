8da2e6606ab2fa638abb972163b403480f679021
Daniel Thapa Magar <mdaniel@insightworkshop.io>
2026-06-16 01:23:12 +0545

    hide category

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,26 +10,9 @@ import {
 const Home: React.FC = () => {
   return (
     <div className="space-y-24">
-      <div
-        className="alert-banner"
-        style={{
-          background: "#fff4e5",
-          color: "#663c00",
-          border: "1px solid #f5c26b",
-          borderRadius: 8,
-          padding: "12px 16px",
-          textAlign: "center",
-          fontSize: 14,
-          fontWeight: 500,
-          marginBottom: "20px",
-        }}
-      >
-        Contact 9861476014 now, the website is still in development and may not
-        be fully working.
-      </div>
       <Hero />
       <FeatureStrip />
-      <CategorySection />
+      {/* <CategorySection /> */}
       <ProductGrid />
       <WhyPipeCleaners />
     </div>
